8201f5c5765bd16dcd3eb84cca10f798e18302a0
Astha Bhandari <asthabhandari21@gmail.com>
2025-07-21 13:04:35 -0700

    es

--- app/lib/config.ts
+++ app/lib/config.ts
@@ -1,16 +1,16 @@
 export const metaData = {
-  baseUrl: "https://nextfolio-template.vercel.app/",
-  title: "Nextfolio",
-  name: "Sirius",
-  ogImage: "/opengraph-image.png",
+  baseUrl: "https://www.asthaspastas.com", // Update with your site base URL
+  title: 'Astha Bhandari', // Update with your site title
+  name: "Astha", // Update with your name
+  ogImage: "/profile.png", // Update with your Open Graph image
   description:
-    "A clean, fast, and lightweight portfolio template built with Next.js, Vercel, and Tailwind CSS for optimal performance.",
+    " welcome to my blog.",
 };
 
 export const socialLinks = {
-  twitter: "https://x.com/1tssirius",
-  github: "https://github.com/1msirius/Nextfolio",
-  instagram: "https://www.instagram.com/",
-  linkedin: "https://www.linkedin.com/",
-  email: "mailto:example@gmail.com",
+  twitter: "https://x.com/asthaspastas", // Update with your Twitter URL
+  github: "https://github.com/asthabhandari/astha-website", // Update with your GitHub URL
+  instagram: "https://www.instagram.com/asthaspastas", // Update with your Instagram URL
+  linkedin: "https://www.linkedin.com/in/astha26", // Update with your LinkedIn URL
+  email: "mailto:asthabhandari21@gmail.com", // Update with your email address
 };
